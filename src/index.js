de3163b54a03d1442e21a3356045cecb1e1ab091
DaniGlass <danielleglass@me.com>
2020-11-11 11:22:17 -0800

    Connect to atlas, architecture

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './pages/App/App';
-import * as serviceWorker from './serviceWorker';
+import App from './Pages/App/App';
+// import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
