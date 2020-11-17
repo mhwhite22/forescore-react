7df40d3a765262c494894e05553d1260f41ebf72
DaniGlass <mhwhite22@gmail.com>
2020-11-17 10:09:46 -0800

    add round & course models

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -5,24 +5,50 @@ import userService from "../../utils/userService";
 import HomePage from "../../Pages/HomePage/HomePage";
 import LandingPage from "../../Pages/LandingPage/LandingPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <Switch>
-        <Route
-        exact path="/"
-        render={() => <LandingPage /> }
-        />
-        <Route
-        exact path="/home"
-        render={() => <HomePage/> }
-        />
-      </Switch>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      days: [],
+      user: userService.getUser(),
+      date: this.getCurrentDate(),
+    };
+  }
+  getCurrentDate() {
+    const today = new Date();
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return today.toLocaleDateString(undefined, options);
+  }
+  handleLogout = () => {
+    userService.logout();
+    this.setState({ user: null });
+  };
 
-    </div>
-  );
+  handleSignupOrLogin = () => {
+    this.setState({ user: userService.getUser() });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route
+          exact path="/"
+          render={() => <LandingPage /> }
+          />
+          <Route
+          exact path="/home"
+          render={() => <HomePage/> }
+          />
+        </Switch>
+
+      </div>
+    );
+  }
 }
 
 export default App;
