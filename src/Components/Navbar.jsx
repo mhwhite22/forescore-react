081c1f6412b2ce65e849f89bf19ba734d794870e
DaniGlass <mhwhite22@gmail.com>
2020-11-17 17:40:43 -0800

    react-materialize, navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import 'materialize-css';
+import { Sidenav, Navbar, Icon } from 'react-materialize';
+import { NavLink } from 'react-router-dom';
 import styles from './Navbar.css';
 
-function Navbar(props) {
+function mainNavbar(props) {
     return (
-        <>
-        <nav className='main-nav sidenav'>
-        <Link to='/'>Rounds</Link>
-        <Link to='/'>Courses</Link>
-        <Link to='/'>New Round</Link>
-        <Link to='/'>New Course</Link>
-        <Link to='/'>Profile</Link> 
-        </nav>
-        </>
+        <Navbar 
+            alignLinks="right"
+            brand={<a className="logo" href="/">Logo</a>}
+            id="mobile-nav"
+            menuIcon={<Icon>menu</Icon>}
+            options={{
+                draggable: true,
+                edge: 'left',
+                inDuration: 250,
+                onCloseEnd: null,
+                onCloseStart: null,
+                onOpenEnd: null,
+                onOpenStart: null,
+                outDuration: 200,
+                preventScrolling: true
+            }}
+            sidenav={<li>Custom node!</li>}
+        >
+            <h1>HI I"M HERE</h1>
+        <NavLink to='/home'>Rounds</NavLink>
+        <NavLink to='/home'>Courses</NavLink>
+        <NavLink to='/home'>New Round</NavLink>
+        <NavLink to='/home'>New Course</NavLink>
+        <NavLink to='/home'>Profile</NavLink> 
+        </Navbar>
     )
 }
 
-export default Navbar;
+export default mainNavbar;
