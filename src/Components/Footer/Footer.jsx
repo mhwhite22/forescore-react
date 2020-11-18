31cab7869030568438774a18d591ccd02daca35f
DaniGlass <mhwhite22@gmail.com>
2020-11-18 12:45:26 -0800

    footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'materialize-css';
+import styles from './Footer.css';
+
+function Footer(props) {
+    return (
+        <div className="main-footer"><h2>ForeScore</h2></div>
+    )
+}
+
+export default Footer;
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Navbar from "../../Components/Navbar";
+import Navbar from "../../Components/Navbar/Navbar";
 import styles from "./HomePage.css"
+import Footer from "../../Components/Footer/Footer";
 
 const HomePage = (props) => {
     return (
         <div className="HomePage">
         <Navbar />
         <h2>Hello, User</h2>
+        <Footer />
         </div>
     )
 }
