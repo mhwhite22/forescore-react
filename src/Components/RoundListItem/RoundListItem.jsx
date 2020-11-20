69f366d9d384345088049a1594187ddd3136bcc0
DaniGlass <mhwhite22@gmail.com>
2020-11-20 12:55:40 -0800

    addendum

--- src/Components/RoundListItem/RoundListItem.jsx
+++ src/Components/RoundListItem/RoundListItem.jsx
@@ -1,3 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './RoundListItem';
+
+function RoundListItem( {round } ) {
+    return (
+        <div className='panel panel-default'>
+            <div className="panel-heading">
+                <h3 className='panel-title'>{round.dateofPlay} at {round.course}</h3>
+            </div>
+            <div className='panel-footer RoundListItem-action-paenl'>
+                <Link
+                    className='btn btn-xs btn-info'
+                    to={{
+                        pathname: '/details',
+                        state: {round}
+                    }}
+                    >Details</Link>
+                <Link>?</Link>
+            </div>
+        </div>
+    );
+}
+
+export default RoundListItem;
--- src/Pages/RoundsPage/RoundsPage.jsx
+++ src/Pages/RoundsPage/RoundsPage.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
+import RoundListItem from '../../Components/RoundListItem/RoundListItem';
 import styles from './RoundsPage.css'
 
 
 function RoundsPage(props) {
     return (
-        <div className="RoundsPage">
-            <Navbar />
+        <>
             <h2>My Rounds</h2>
+            <Navbar />
             <div className='RoundsPage-grid'>
-            </div>
+                {props.rounds.map(round =>
+                    <RoundListItem
+                    round={round}
+                    key={round._id}
+                    />
+                )}
         </div>
-    )
+        </>
+    );
 }
 
 export default RoundsPage;
