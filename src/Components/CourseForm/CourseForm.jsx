733ff99efd57b0a25c50a5b365ac59152bce85e2
DaniGlass <mhwhite22@gmail.com>
2020-12-01 20:23:58 -0800

    finish course form

--- src/Components/CourseForm/CourseForm.jsx
+++ src/Components/CourseForm/CourseForm.jsx
@@ -37,7 +37,79 @@ class CourseForm extends Component {
       render() {
           return (
               <div className="CourseForm">
-                  <h2>courseform</h2>
+                  <header className="header-footer">Add A New Course</header>
+                  <form
+                    ref={this.formRef}
+                    className="form-horizontal"
+                    onSubmit={this.handleSubmit}
+                    >
+                      <div className="form-group">
+                        <div className="col-sm-12">
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Course Name"
+                            value={this.state.formData.name}
+                            onChange={this.handleChange}
+                          />
+                        </div>
+                      </div>
+                      <div className="form-group">
+                        <div className="col-sm-12">
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Course Location"
+                            value={this.state.formData.location}
+                            onChange={this.handleChange}
+                          />
+                        </div>
+                      </div>
+                      <div className="form-group">
+                        <div className="col-sm-12">
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Par"
+                            value={this.state.formData.par}
+                            onChange={this.handleChange}
+                          />
+                        </div>
+                      </div>
+                      <div className="form-group">
+                        <div className="col-sm-12">
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Yardage"
+                            value={this.state.formData.yardage}
+                            onChange={this.handleChange}
+                          />
+                        </div>
+                      </div>
+                      <div className="form-group">
+                        <div className="col-sm-12">
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Slope"
+                            value={this.state.formData.slope}
+                            onChange={this.handleChange}
+                          />
+                        </div>
+                      </div>
+                    </form>
+                    <div className="form-group">
+                        <div className="col-sm-12">
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Holes"
+                            value={this.state.formData.numHoles}
+                            onChange={this.handleChange}
+                          />
+                        </div>
+                      </div>
               </div>
           )
       }
