1aeb829e7a2ebcd409285eed3565f52ebe679e7d
DaniGlass <mhwhite22@gmail.com>
2020-12-02 10:42:42 -0800

    add back-end routes, controllers for course creation

---
+++ routes/api/courses.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const coursesCtrl = require('../../controllers/api/rounds');
+
+router.use(require('../../config/auth'));
+
+router.post('/', roundsCtrl.create);
+router.get('/', roundsCtrl.index);
+
+function checkAuth(req, res, next) {
+    if (req.user) return next();
+    return res.status(401).json({ msg: 'Not Authorized'});
+}
+
+module.exports = router;
