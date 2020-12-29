cad8c60fba28277d41de52076f0d106a610d565d
mhwhite22 <mhwhite22@gmail.com>
2020-12-29 12:48:53 -0800

    change procfile

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
 app.use(express.static(path.join(__dirname, "build")));
 
-// Put API Routes here
+// API Routes here
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/courses", require("./routes/api/courses"));
 app.use("/api/rounds", require("./routes/api/rounds"));
